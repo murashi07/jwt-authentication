e8d4bd6480b4e3fe7f75278353b476875306cee1
murashi07 <yvanmurashi@gmail.com>
2025-01-03 14:14:06 +0200

    jwt configurations

--- src/main/java/com/jwt/jwt_authentication/model/User.java
+++ src/main/java/com/jwt/jwt_authentication/model/User.java
@@ -1,2 +1,70 @@
-package com.jwt.jwt_authentication.model;public class User {
+package com.jwt.jwt_authentication.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+
+@Entity
+@Table(name="users")
+@Data
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private boolean enabled;
+
+    @Column(name = "verification_code")
+    private String  verificationCode;
+    @Column(name = "verification_expiration")
+    private LocalDateTime verificationCodeExpiresAt;
+
+    public User() {
+    }
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired(){
+        return  true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked(){
+        return true;
+    }
+    @Override
+    public  boolean isCredentialsNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled(){
+        return enabled;
+    }
 }
